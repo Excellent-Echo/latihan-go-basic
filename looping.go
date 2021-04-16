8856d238f05cc39c579e3d1bea32e7f32e733cc9
marwanjuna <mrwnolii@gmail.com>
2021-04-16 15:45:49 +0700

    deleted print i in looping.go

--- looping.go
+++ looping.go
@@ -6,13 +6,13 @@ func main() {
 	// your code here
 	for i := 1; i <= 100; i++ {
 		if i%2 == 0 {
-			fmt.Println("Buzz", i)
+			fmt.Println("Buzz")
 		}
 		if i%3 == 0 {
-			fmt.Println("Fuzz", i)
+			fmt.Println("Fuzz")
 		}
 		if i%5 == 0 {
-			fmt.Println("BuzzFuzz", i)
+			fmt.Println("BuzzFuzz")
 		}
 	}
 }
