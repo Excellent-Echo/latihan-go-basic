7db93f8210ee829b143b04f40fc808a9bc73b31a
marwanjuna <mrwnolii@gmail.com>
2021-04-16 09:46:03 +0700

    looping finished

--- looping.go
+++ looping.go
@@ -1,6 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	// your code here
-
+	for i := 1; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println("Buzz", i)
+		} else if i%3 == 0 {
+			fmt.Println("Fuzz", i)
+		} else if i%5 == 0 {
+			fmt.Println("BuzzFuzz", i)
+		} else {
+			continue
+		}
+	}
 }
