5b1b297925acdddde104fc616e1f91c337d2828f
andryansyhh <andryansyhh@gmail.com>
2021-04-16 11:32:32 +0700

    done simple calc

--- simpleCalc.go
+++ simpleCalc.go
@@ -4,16 +4,28 @@ import "fmt"
 
 func main() {
 	var (
-		num1, num2 float32
+		num1, num2 int
 		method     string
 	)
 	fmt.Print("input calculator : ")
 	fmt.Scanf("%d %v %d", &num1, &method, &num2)
+	
 
 	//your code here
 
 	switch method{
-	case 1:
+	case "+":
 		fmt.Println("hasil: ", num1+num2)
+	case "-" :
+		fmt.Println("hasil: ", num1-num2)
+	case "%" :
+		fmt.Println("hasil: ", num1%num2)
+	case "*" :
+		fmt.Println("hasil: ", num1*num2)
+	case "/" :
+		fmt.Println("hasil: ", num1/num2)
+	default:
+		fmt.Println("error operasi yang dimasukkan salah!!!!!!!!!!!")
+	}
 
 }
