af27c02c9fb3db2f863ae51d9d8dfdb230413ce3
danangtomo <ajidanang9@gmail.com>
2021-04-16 13:11:40 +0700

    update looping

--- looping.go
+++ looping.go
@@ -5,12 +5,16 @@ import "fmt"
 func main() {
 	// your code here
 	for numberLoop := 1; numberLoop <= 100; numberLoop++ {
-		if numberLoop % 2 == 0 {
-			fmt.Println("Buzz")
-		} else if numberLoop % 3 == 0 {
-			fmt.Println("Fuzz")
+		if numberLoop % 5 == 0 && numberLoop % 3 == 0 && numberLoop % 2 == 0 {
+			fmt.Println("BuzzFuzz Fuzz Buzz")
+		} else if numberLoop % 5 == 0 && numberLoop % 3 == 0  {
+			fmt.Println("BuzzFuzz Fuzz")
 		} else if numberLoop % 5 == 0 {
 			fmt.Println("BuzzFuzz")
+		} else if numberLoop % 3 == 0 {
+			fmt.Println("Fuzz")
+		} else if numberLoop % 2 == 0 {
+			fmt.Println("Buzz")
 		}
 	}
 }
