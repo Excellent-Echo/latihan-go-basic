9f183ac66093fa730ed4bec71af07abf3b9d029b
helyus1412 <helyus1412@gmail.com>
2021-04-16 10:02:05 +0700

    selesai, benarkah mas ?

--- looping.go
+++ looping.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	// your code here
-
+	for i := 1; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fuzz")
+		} else if i%5 == 0 {
+			fmt.Println("BuzzFuzz")
+		}
+	}
 }
