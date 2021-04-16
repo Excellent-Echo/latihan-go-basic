4827a19a0a3497fdbcab3adc17b00888143b688a
mohammedazez <muhamadaziz047@gmail.com>
2021-04-16 19:43:33 +0700

    looping done

--- looping.go
+++ looping.go
@@ -1,6 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	// your code here
-
+	for i := 0;  i <= 100; i++ {
+		
+		// dapat bagi 2 show buzz
+		if i % 2 == 0 {
+			fmt.Println(i, ": is buzz")
+			// dapat bagi 3 show fuzz
+			} else if i % 3 == 0 {
+				fmt.Println(i, ": is fuzz")
+		// dapat bagi 5 show BuzzFuzz 
+		} else if i % 5 == 0 {
+			fmt.Println(i, ": is BuzzFuzz")
+		}
+	}
 }
