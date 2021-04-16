6b4068bc8f0186c2aed0f8cf0c266541ecae5623
danangtomo <ajidanang9@gmail.com>
2021-04-16 10:14:46 +0700

    looping buzz fuzz buzzFuzz

--- looping.go
+++ looping.go
@@ -1,6 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	// your code here
-
+	for numberLoop := 1; numberLoop <= 100; numberLoop++ {
+		if numberLoop % 2 == 0 {
+			fmt.Println("Buzz")
+		} else if numberLoop % 3 == 0 {
+			fmt.Println("Fuzz")
+		} else if numberLoop % 5 == 0 {
+			fmt.Println("BuzzFuzz")
+		} else {
+			fmt.Println(numberLoop)
+		}
+	}
 }
