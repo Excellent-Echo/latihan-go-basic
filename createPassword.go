d39bdffe3eb67b375a7a17d53b852abdc248caa0
Ah Rifqi Ubaidilah <42555735+rifqi23ahmad@users.noreply.github.com>
2021-04-16 14:13:10 +0700

    selesai

--- createPassword.go
+++ createPassword.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,4 +16,17 @@ func main() {
 
 	//your code here
 
+	if len(name) < 5 {
+		fmt.Println("Error ! Anda harus memasuskan minimal 5 karakter ")
+	} else {
+		var revers string
+		var result string
+		var first string = string(name[0])
+		var last string = string(name[len(name)-1])
+		for i := len(name) - 1; i >= 0; i-- {
+			revers += string(name[i])
+		}
+		result = strings.ToUpper(first) + revers + strings.ToUpper(last) + strconv.Itoa(len(name))
+		fmt.Println(result)
+	}
 }
