163ad709238c4c44ceca84dec10a853c52b3adf2
Ah Rifqi Ubaidilah <42555735+rifqi23ahmad@users.noreply.github.com>
2021-04-16 11:42:40 +0700

    finish 2 soal

--- looping.go
+++ looping.go
@@ -1,6 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
 	// your code here
+	for i := 1; i <= 100; i++ {
 
+		if i%2 == 0 {
+
+			fmt.Printf("Buzz")
+		}
+		if i%3 == 0 {
+
+			fmt.Printf("Fuzz")
+		}
+
+		if i%5 == 0 {
+			fmt.Println("BuzzFuzz")
+		}
+
+		if i%2 != 0 && i%5 != 0 && i%3 != 0 {
+			fmt.Printf("%d", i)
+		}
+		fmt.Printf("\n")
+
+	}
 }
--- simpleCalc.go
+++ simpleCalc.go
@@ -1,14 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var (
-		num1, num2 float32
-		method     string
+		num1, num2, hasil int32
+		method            string
 	)
 	fmt.Print("input calculator : ")
 	fmt.Scanf("%d %v %d", &num1, &method, &num2)
 
 	//your code here
+	switch method {
+	case "+":
+		hasil = num1 + num2
+		fmt.Printf("Hasil: %d", hasil)
+	case "-":
+		hasil = num1 - num2
+		fmt.Printf("Hasil: %d", hasil)
+	case "/":
+		hasil = num1 / num2
+		fmt.Printf("Hasil: %d", hasil)
+	case "X":
+		hasil = num1 * num2
+		fmt.Printf("Hasil: %d", hasil)
+	default:
+		fmt.Println("input salah")
+	}
+
 }
