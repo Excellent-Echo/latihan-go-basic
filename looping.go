65f8f811d7bf2e61ac80e0749f67e897d7d2db95
ardhisr <radika.kerenz1@gmail.com>
2021-04-16 10:10:34 +0700

    done looping.go

--- looping.go
+++ looping.go
@@ -1,6 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	// your code here
-
+	// di bagi 2 = buz
+	// di bagi 3 = fuzz
+	// di bagi 5 = buzzfuzz
+	for i := 1; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println("Buzz")
+		}
+		if i%3 == 0 {
+			fmt.Println("Fuzz")
+		}
+		if i%5 == 0 {
+			fmt.Println("BuzzFuzz")
+		}
+	}
 }
