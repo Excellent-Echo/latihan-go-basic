30ddcc92a5bda55866236c972e6a9fedbe9c41e8
Angga Prasetya W <agpw.id@gmail.com>
2021-04-16 11:38:29 +0800

    looping.go finished

--- looping.go
+++ looping.go
@@ -1,6 +1,23 @@
 package main
 
-func main() {
-	// your code here
+import "fmt"
 
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%2 == 0 && i%3 == 0 {
+			fmt.Println(i, "Buzz", "Fuzz")
+		} else if i%2 == 0 && i%5 == 0 {
+			fmt.Println(i, "Buzz", "FuzzBuzz")
+		} else if i%3 == 0 && i%5 == 0 {
+			fmt.Println(i, "Fuzz", "FuzzBuzz")
+		} else if i%2 == 0 {
+			fmt.Println(i, "Buzz")
+		} else if i%3 == 0 {
+			fmt.Println(i, "Fuzz")
+		} else if i%5 == 0 {
+			fmt.Println(i, "FuzzBuzz")
+		} else {
+			continue
+		}
+	}
 }
