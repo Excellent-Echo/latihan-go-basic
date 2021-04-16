c51452a0c4341fccb6a8d6607a99c736b68f68cc
ammarteuku05 <ammarteuku09@gmail.com>
2021-04-16 11:05:37 +0700

    simpleCalc selesai

--- simpleCalc.go
+++ simpleCalc.go
@@ -1,14 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var (
-		num1, num2 float32
-		method     string
+		num1, num2, results int
+		method              string
 	)
+	fmt.Printf(method)
 	fmt.Print("input calculator : ")
 	fmt.Scanf("%d %v %d", &num1, &method, &num2)
-
 	//your code here
+
+	if method == "+" {
+		results = num1 + num2
+		fmt.Println("Hasil = ", results)
+	} else if method == "-" {
+		fmt.Println("Hasil = ", num1-num2)
+	} else if method == "*" {
+		fmt.Println("Hasil = ", num1*num2)
+	} else if method == "/" {
+		fmt.Println("Hasil = ", num1/num2)
+	} else if method == "%" {
+		fmt.Println("Hasil = ", num1%num2)
+	} else {
+		fmt.Println("Format yang anda masukkan salah")
+	}
+
 }
