968b1d796d7d7efe523d03cf973fa568662afd09
ardhisr <radika.kerenz1@gmail.com>
2021-04-16 10:44:19 +0700

    done simpleCalc.go

--- simpleCalc.go
+++ simpleCalc.go
@@ -4,11 +4,26 @@ import "fmt"
 
 func main() {
 	var (
-		num1, num2 float32
+		num1, num2 int
 		method     string
 	)
 	fmt.Print("input calculator : ")
-	fmt.Scanf("%d %v %d", &num1, &method, &num2)
+	fmt.Scanf("%d %s %d", &num1, &method, &num2)
 
 	//your code here
+	//state := true
+
+	//for state == true {
+	if method == "+" {
+		fmt.Println(num1 + num2)
+	} else if method == "-" {
+		fmt.Println(num1 - num2)
+	} else if method == "/" {
+		fmt.Println(num1 / num2)
+	} else if method == "%" {
+		fmt.Println(num1 % num2)
+	} //else {
+	//	fmt.Println("error operasi yang dimasukan salah")
+	//}
+	//}
 }
