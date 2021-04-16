a1740af89e49f7efedd6a1ca5bf3fdb64ebc2820
mohammedazez <muhamadaziz047@gmail.com>
2021-04-16 19:45:00 +0700

    simpleCalc done

--- simpleCalc.go
+++ simpleCalc.go
@@ -11,4 +11,23 @@ func main() {
 	fmt.Scanf("%d %v %d", &num1, &method, &num2)
 
 	//your code here
+	switch method {
+		case "+": 
+			result := num1 + num2
+			fmt.Println("Hasil penjumlahan :", result)
+		case "-":
+			result := num1 - num2
+			fmt.Println("Hasil pengurangan :", result)
+		case "/":
+			result := num1 / num2
+			fmt.Println("Hasil pembagian :", result)
+		case "*": 
+			result := num1 * num2
+			fmt.Println("Hasil perkalian :", result)
+		case "%": 
+			result := num1 % num2
+			fmt.Println("Hasil modulus :", result)
+		default:
+			fmt.Println("error operasi yang dimasukkan salah!")
+		}
 }
