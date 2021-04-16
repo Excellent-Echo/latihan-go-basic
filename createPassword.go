6ff6795b0834882b0d6f1db962ce76f60b339127
sonex23 <sonaccx7@gmail.com>
2021-04-16 14:25:07 +0700

    done all

--- createPassword.go
+++ createPassword.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,5 +15,17 @@ func main() {
 	fmt.Scanf("%s", &name)
 
 	//your code here
-
+	nameLen := len(name)
+	pass := ""
+	if nameLen <= 5 {
+		fmt.Println("Error, nama minimal 5 karakter")
+	} else {
+		for i := nameLen - 1; i >= 0; i-- {
+			pass += string(name[i])
+		}
+		awal := strings.ToUpper(string(name[0]))
+		akhir := strings.ToUpper(string(name[nameLen-1]))
+		hasil := awal + pass + akhir + strconv.Itoa(nameLen)
+		fmt.Println(hasil)
+	}
 }
