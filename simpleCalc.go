198e032ddd8e42337d3c81eb8ca5001e31a49f32
Muhammad Fauzul Hanif <50539720+efzedel16@users.noreply.github.com>
2021-04-18 01:34:03 +0700

    simpleCalc.go has been completed

--- simpleCalc.go
+++ simpleCalc.go
@@ -5,10 +5,24 @@ import "fmt"
 func main() {
 	var (
 		num1, num2 int
-		method     string
+		method string
 	)
-	fmt.Print("input calculator : ")
+	fmt.Println("Kalkulator : ")
 	fmt.Scanf("%d %v %d", &num1, &method, &num2)
 
 	//your code here
+	switch method {
+	case "+":
+		fmt.Printf("Hasil : %d", num1 + num2)
+	case "-":
+		fmt.Printf("Hasil : %d", num1 - num2)
+	case "*":
+		fmt.Printf("Hasil : %d", num1 * num2)
+	case "/":
+		fmt.Printf("Hasil : %d", num1 / num2)
+	case "%":
+		fmt.Printf("Hasil : %d", num1 % num2)
+	default:
+		fmt.Println("Masukan salah!")
+	}
 }
