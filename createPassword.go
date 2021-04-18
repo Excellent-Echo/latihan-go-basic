a265f46ea13e6926b29d47aa8bc94bd1b8718f51
efzedel16 <efzedel16@outlook.com>
2021-04-19 00:39:21 +0700

    createPassword.go has been completed

--- createPassword.go
+++ createPassword.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	var (
-		name string
+		name, reverse string
 	)
 
-	fmt.Print("masukkan nama untuk diubah menjadi password : ")
+	fmt.Print("Enter a name to be changed to a password (min 5 chars): ")
 	fmt.Scanf("%s", &name)
 
 	//your code here
+	if len(name) < 5 {
+		fmt.Println("Error! '",name,"' less than 5 chars")
+	} else {
+		firstChar := string(name[0])
+		lastChar := string(name[len(name) - 1])
+		amountChar := strconv.Itoa(len(name))
 
+		for i := len(name) - 1; i >= 0 ; i-- {
+			reverse += string(name[i])
+		}
+
+		result := strings.ToUpper(firstChar) + reverse + strings.ToUpper(lastChar) + amountChar
+		fmt.Println(result)
+	}
 }
