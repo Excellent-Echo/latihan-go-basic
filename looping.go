74617d9c90a17aee34934b91c2d3e179c4510a12
efzedel16 <efzedel16@outlook.com>
2021-04-18 10:51:40 +0700

    looping.go has been completed

--- looping.go
+++ looping.go
@@ -1,6 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 	// your code here
-
+	for i := 1; i <= 100; i++ {
+		if i % 5 == 0 && i % 3 == 0 && i % 2 == 0 {
+			fmt.Println(i, "= Buzz Fuzz BuzzFuzz")
+		} else if i % 5 == 0 && i % 3 == 0 {
+			fmt.Println(i, "= Fuzz BuzzFuzz")
+		} else if i % 5 == 0 && i % 2 == 0 {
+			fmt.Println(i, "= Buzz BuzzFuzz")
+		} else if i % 5 == 0 {
+			fmt.Println(i, "= BuzzFuzz")
+		} else if i % 5 == 0 {
+			fmt.Println(i, "= BuzzFuzz")
+		} else if i % 3 == 0 {
+			fmt.Println(i, "= Fuzz")
+		} else if i % 2 == 0 {
+			fmt.Println(i, "= Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
